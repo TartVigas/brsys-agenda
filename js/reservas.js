a0025015ec30d3ce789b265f530c7283d900aadc
TartVigas <thiagobarrosvend@gmail.com>
2026-02-02 09:24:45 -0300

    Update reservas.js

--- js/reservas.js
+++ js/reservas.js
@@ -1,1 +1,13 @@
+import { requireAuth } from "./auth.js";
+import { loadHotelContext, renderHotelBadge } from "./state.js";
+
+const user = await requireAuth();
+const ctx = await loadHotelContext();
+
+if (!ctx) {
+  // não tem hotel criado ainda -> manda pro cadastro
+  window.location.replace("/cadastro.html");
+}
+
+renderHotelBadge(ctx);
 
