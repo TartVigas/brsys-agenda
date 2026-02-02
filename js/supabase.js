5c3b440cab261ce9b5d6ac360aed8989db444c2c
TartVigas <thiagobarrosvend@gmail.com>
2026-02-02 05:12:35 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -5,9 +5,8 @@ export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiO
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
+    detectSessionInUrl: true,
     persistSession: true,
     autoRefreshToken: true,
-    detectSessionInUrl: true
-  }
+  },
 });
-
