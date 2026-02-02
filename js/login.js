9728fa67cd323af8a6aee3b2a9b9e18ad67a02a2
TartVigas <thiagobarrosvend@gmail.com>
2026-02-02 04:41:25 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,8 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // 🔑 pegue em: Supabase > Project Settings > API
-const SUPABASE_URL = "https://https://qxocjsthqflffgqbrhe.supabase.co
-.supabase.co";
+const SUPABASE_URL = "https://https://qxocjsthqflffgqbrhe.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4b2Nqc3RocWZsZ2ZmZ3FicmhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNTYyMzIsImV4cCI6MjA3ODYzMjIzMn0.DH1LQ1N7tBHMyKlBpuzLU69GBeMhoq-z92CnX0i-7jY";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
